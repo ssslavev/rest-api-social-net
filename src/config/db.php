a44284e19d086bc43a2cf94faf495e18bb9302da
ssslavev <s.s.slavev@hotmail.com>
2019-03-07 00:20:48 +0200

    fix

--- src/config/db.php
+++ src/config/db.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin:  http://localhost:4200");
-header("Access-Control-Allow-Methods: PUT, GET, POST");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header("Access-Control-Allow-Credentials: true");
+header ("Access-Control-Allow-Origin: *");
+header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header ("Access-Control-Allow-Headers: *");
 
 class Db {
     //private $dbhost = 'localhost';
