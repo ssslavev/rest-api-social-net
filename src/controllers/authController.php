8df37be83e9dea3648652322e2fc5efe5442049b
ssslavev <s.s.slavev@hotmail.com>
2019-05-06 13:19:48 +0300

    Change in login logic

--- src/controllers/authController.php
+++ src/controllers/authController.php
@@ -1,68 +1,62 @@
-<?php 
-use \Psr\Http\Message\ServerRequestInterface as Request;
+<?php
 use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
 
 require '../src/data/authData.php';
 require '../src/TokenGenerator.php';
 
+class AuthController
+{
 
+    public function register(Request $request, Response $response, array $args)
+    {
 
+        $name = $request->getParam('name');
+        $password = password_hash($request->getParam('password'), PASSWORD_DEFAULT);
+        $firstName = $request->getParam('firstName');
+        $lastName = $request->getParam('lastName');
+        $email = $request->getParam('email');
 
-class AuthController {
+        $userToAdd = ['name' => $name,
+            'password' => $password,
+            'firstName' => $firstName,
+            'lastName' => $lastName,
+            'email' => $email];
 
-
-    public function register(Request $request, Response $response, array $args) {
-         
-         $name = $request->getParam('name');
-         $password =  password_hash($request->getParam('password'), PASSWORD_DEFAULT);
-         $firstName = $request->getParam('firstName');
-         $lastName = $request->getParam('lastName');
-         $email = $request->getParam('email');
-         
-         $userToAdd = ['name'=>$name,
-                       'password'=>$password,
-                        'firstName'=>$firstName,
-                        'lastName'=>$lastName,
-                        'email'=>$email];
-
-         $authData = new AuthData();
+        $authData = new AuthData();
 
         try {
-           $data =   $authData->add($userToAdd);
-           return $response->withjson($data);
+            $data = $authData->add($userToAdd);
+            return $response->withjson($data);
         } catch (Exception $e) {
-            $error = ["message"=> $e->getMessage()];
-            return $response->withJson( $error);
+            $error = ["message" => $e->getMessage()];
+            return $response->withJson($error);
         }
 
-         
     }
 
-    public function login(Request $request, Response $response, array $args) {
+    public function login(Request $request, Response $response, array $args)
+    {
 
         $name = $request->getParam('name');
         $password = $request->getParam('password');
 
-        $userToLogIn = ['name'=>$name, 'password'=>$password];
+        $userToLogIn = ['name' => $name, 'password' => $password];
 
         $authData = new AuthData();
         $loggedUser = $authData->login($userToLogIn);
 
-
         if ($loggedUser) {
             $jwt = TokenGenerator::getToken($loggedUser);
+            unset($loggedUser['password']);
             return $response->withJson([
                 "user" => $loggedUser,
-                "token" => $jwt
+                "token" => $jwt,
             ]);
         } else {
             return $response->withStatus(500);
         }
-       
-        
-        
-        
-        
+
     }
 
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 
 require_once '../src/controllers/authController.php';
 require_once '../src/controllers/usersController.php';
@@ -7,29 +6,24 @@ require_once '../src/controllers/postsController.php';
 require_once '../src/controllers/friendReqController.php';
 require_once '../src/controllers/imagesController.php';
 
-
 $container = new \Slim\Container;
 $app = new \Slim\App($container);
-
-
 
 $app->add(new Tuupola\Middleware\JwtAuthentication([
     "secure" => false,
     "secret" => "supersecretkeyyoushouldnotcommittogithub",
-    "path"=> "/",
-    "ignore"=>["/api/users/login",
-                "/api/users/register"]
+    "path" => "/",
+    "ignore" => ["/api/users/login",
+        "/api/users/register"],
 ]));
-
-
 
 $app->post('/api/users/register', '\AuthController:register');
 $app->post('/api/users/login', '\AuthController:login');
 $app->get('/api/users', '\UsersController:getAllUsers');
-$app->get('/api/users/{id}', '\UsersController:getUser' );
+$app->get('/api/users/{id}', '\UsersController:getUser');
 $app->post('/api/posts', '\PostsController:addPost');
 $app->get('/api/posts', '\PostsController:getAllPosts');
-$app->get('/api/posts/{id}', '\PostsController:getPostById'); 
+$app->get('/api/posts/{id}', '\PostsController:getPostById');
 $app->get('/api/usersandposts', '\UsersController:getUsersWithPosts');
 $app->get('/api/users/posts/{id}', '\PostsController:getPostsByUserId');
 $app->post('/api/users/friends/request', '\FriendReqController:sendFriendReq');
@@ -37,39 +31,30 @@ $app->post('/api/users/friends/fromreq', '\FriendReqController:getFromReq');
 $app->post('/api/users/friends/toreq', '\FriendReqController:getToReq');
 $app->post('/api/users/friends/acceptreq', '\FriendReqController:acceptReq');
 $app->post('/api/users/friends', '\FriendReqController:getFriends');
-$app->post('/api/users/{id}/pictures', '\ImagesController:uploadImage' );
+$app->post('/api/users/{id}/pictures', '\ImagesController:uploadImage');
 $app->get('/api/users/{id}/images', '\ImagesController:getImagesByuserId');
 $app->get('/api/users/{id}/friends/all-requests', '\FriendReqController:getAllRequests');
 $app->post('/api/users/friends/requests', '\FriendReqController:deleteRequest');
 $app->post('/api/users/friendsList', '\FriendReqController:getFriendsList');
 
-
-
 $container['upload_directory'] = __DIR__ . '/uploads';
 
-$container['AuthController'] = function() {
+$container['AuthController'] = function () {
     return new AuthController();
 };
 
-
-$container['UsersController'] = function() {
+$container['UsersController'] = function () {
     return new UsersController();
 };
 
-
-$container['postsController'] = function() {
+$container['postsController'] = function () {
     return new postsController();
 };
 
-
-$container['friendReqController'] = function() {
+$container['friendReqController'] = function () {
     return new friendReqController();
 };
 
-
-$container['imagesController'] = function() {
+$container['imagesController'] = function () {
     return new ImagesController();
 };
-
-
-
